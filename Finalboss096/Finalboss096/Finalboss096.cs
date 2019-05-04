cdb1cbf0330b347da2196a4088053721f3c19df5
albertinchu Martin Palomino <albertinchu@hotmail.com>
2019-05-04 02:55:11 +0200

    Fin

---
+++ Finalboss096/Finalboss096/Finalboss096.cs
@@ -0,0 +1,43 @@
+﻿using Smod2;
+using Smod2.Attributes;
+namespace Finalboss096
+{
+    [PluginDetails(
+        author = "Albertinchu ",
+        name = "Finalboss096",
+        description = "un 096 con 40000 de vida, muchos jugadores...",
+        id = "albertinchu.gamemode.FB096",
+        version = "1.0.0",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    public class Finalboss096 : Plugin
+    {
+
+        public override void OnDisable()
+        {
+            this.Info("Finalboss096 - Desactivado");
+        }
+
+        public override void OnEnable()
+        {
+            Info("Finalboss096 - activado.");
+        }
+
+        public override void Register()
+        {
+            GamemodeManager.Manager.RegisterMode(this);
+            
+            this.AddEventHandlers(new PlayersEvents(this));
+
+        }
+        public void RefreshConfig()
+        {
+
+
+        }
+    }
+
+}
+
---
+++ Finalboss096/Finalboss096/PlayersEvents.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Smod2;
+using Smod2.EventHandlers;
+using Smod2.API;
+using Smod2.Events;
+using Smod2.EventSystem.Events;
+
+namespace Finalboss096
+{
+    partial class PlayersEvents : IEventHandlerSetRole, IEventHandlerWaitingForPlayers, IEventHandlerSetConfig, IEventHandlerSetSCPConfig, IEventHandlerPlayerHurt,
+    IEventHandlerRoundEnd, IEventHandlerRoundStart
+        {
+        private Finalboss096 plugin;
+        public PlayersEvents(Finalboss096 plugin)
+        {
+            this.plugin = plugin;
+        }
+        static Dictionary<int, float> Jugadores = new Dictionary<int, float>();
+        float var1, var2;
+        int MVP;
+        string Name;
+        Vector posboss = PluginManager.Manager.Server.Map.GetSpawnPoints(Role.CHAOS_INSURGENCY).First();
+        Vector posjgdrs = PluginManager.Manager.Server.Map.GetSpawnPoints(Role.NTF_COMMANDER).First();
+
+        public void OnSetConfig(SetConfigEvent ev)
+        {
+            switch (ev.Key)
+            {
+                
+                   
+                case "auto_warhead_start":
+                    ev.Value = 1800;
+                    break;
+                case "auto_warhead_start_lock":
+                    ev.Value = false;
+                    break;
+                case "default_item_classd":
+                    ev.Value = new int[] { 25,24,25,25,30,14,14 };
+                    break;
+                case "default_item_scientist":
+                    ev.Value = new int[] { 21,20,25,26,16,14,25 }; 
+                    break;
+                case "minimum_MTF_time_to_spawn":
+                    ev.Value = 210;
+                    break;
+                case "maximum_MTF_time_to_spawn":
+                    ev.Value = 210;
+                    break;
+            }
+        }
+
+        public void OnSetRole(PlayerSetRoleEvent ev)
+        {
+
+            if((ev.Player.TeamRole.Role == Role.CHAOS_INSURGENCY)||(ev.Player.TeamRole.Role == Role.FACILITY_GUARD))
+            {
+                ev.Player.ChangeRole(Role.CLASSD);
+                ev.Player.Teleport(posjgdrs);
+            }
+            if((ev.Player.TeamRole.Role == Role.CLASSD)||(ev.Player.TeamRole.Role == Role.SCIENTIST))
+            {
+                ev.Player.Teleport(posjgdrs);
+            }
+            if(ev.Player.TeamRole.Role == Role.SCP_096) { ev.Player.AddHealth((ev.Player.GetHealth() * 20)); }
+            if(ev.Player.TeamRole.Team != Team.SCP) { ev.Player.SetAmmo(AmmoType.DROPPED_5, 600); ev.Player.SetAmmo(AmmoType.DROPPED_7, 600);
+                ev.Player.SetAmmo(AmmoType.DROPPED_9, 600);
+            }
+        }
+
+        public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
+        {
+            plugin.RefreshConfig();
+        }
+
+        public void OnSetSCPConfig(SetSCPConfigEvent ev)
+        {
+            ev.Ban049 = true;
+            ev.Ban079 = true;
+            ev.Ban106 = true;
+            ev.Ban173 = true;
+            ev.Ban939_53 = true;
+            ev.Ban939_89 = true;
+        }
+
+        public void OnPlayerHurt(PlayerHurtEvent ev)
+        {
+           if(ev.Player.TeamRole.Role == Role.SCP_096)
+            {
+                Jugadores[ev.Player.PlayerId] = Jugadores[ev.Player.PlayerId] + ev.Damage;
+            }
+        }
+
+        public void OnRoundEnd(RoundEndEvent ev)
+        {
+            
+            foreach (KeyValuePair<int, float> key in Jugadores)
+            {
+                var1 = key.Value;
+                if (var1 > var2) { var2 = var1; MVP = key.Key; }
+            }
+            foreach (Player player in PluginManager.Manager.Server.GetPlayers())
+            {
+                if(player.PlayerId == MVP)
+                {
+                    Name = player.Name;
+                }
+            }
+            PluginManager.Manager.Server.Map.Broadcast(10, "El Mejor jugador ha sido " + Name +" El daño causado" + var2.ToString(),false);
+        }
+
+        public void OnRoundStart(RoundStartEvent ev)
+        {
+           foreach(Player player in PluginManager.Manager.Server.GetPlayers())
+            {
+                if(player.TeamRole.Role != Role.SCP_096)
+                {
+                    Jugadores.Add(player.PlayerId, 0);
+                }
+
+            }
+        }
+    }
+}
